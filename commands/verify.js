9f603019ddbecf68b53b0c4d551784499fa60263
SetchiChan <35151486+SetchiChan@users.noreply.github.com>
2018-02-14 21:09:07 -0500

    Create verify.js

---
+++ commands/verify.js
@@ -0,0 +1,11 @@
+const Discord = require("discord.js");
+
+module.exports.run = async (bot, message, args) => {
+
+    message.reply("An error has occured while running this commmand. Please contact staff for help!");
+
+}
+
+module.exports.help = { 
+    name: "verify"
+}
