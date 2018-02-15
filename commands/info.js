500b8dba05e3e68a00505b1d4f2456b5d75c647e
SetchiChan <35151486+SetchiChan@users.noreply.github.com>
2018-02-14 20:19:05 -0500

    Update info.js

--- commands/info.js
+++ commands/info.js
@@ -2,21 +2,7 @@ const Discord = require("discord.js");
 
 module.exports.run = async (bot, message, args) => {
 
-    const collector = message.createReactionCollector(reaction, user);
-
-    const embed = new Discord.RichEmbed()
-    .setTitle("")
-    .setAuthor("Heaven Shadow Commands", "https://i.imgur.com/7QfhxU1.png")
-    .setColor(10038562)
-    .setDescription("")
-    .setFooter("Commands available for: Guardian Member")
-    .setTimestamp()
-    .addField("' or 'Help", "Lists Server Commands")
-    .addField("'Info", "Lists server information.", true)
-    .addField("'Pref", "Pref changes your prefered lane roles. [Limit 2 Roles]", true)
-    .addField("'Rankupdate", "Updates your current Discord profile with current LOL statistics.", true)
-
-    message.author.send({embed});
+  message.reply("An error has occured while processing this command. Please contact SetchiChan or staff for help!");
 
 }
 
